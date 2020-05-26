4d608890038c2d8a7d4ce93054035543aa5bb38e
David Hoeppner <dhoeppne@ualberta.ca>
2020-05-26 16:29:21 -0600

    adding dependencies

---
+++ listener.py
@@ -0,0 +1,8 @@
+const page = require('webpage').create();
+page.open('http://example.com', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+    page.render('example.png');
+  }
+  phantom.exit();
+});
